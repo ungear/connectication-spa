efab6a830d32d7da01786ec97047263e6dd59189
Mikhail Istomin <mikhail_istomin@epam.com>
2022-11-16 23:29:20 +0400

    basic links in the actions menu

--- src/app/core/actions-menu/actions-menu.component.ts
+++ src/app/core/actions-menu/actions-menu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {ConnecticationStore} from '../../store/connectication-store.interface';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-actions-menu',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actions-menu.component.scss']
 })
 export class ActionsMenuComponent implements OnInit {
-
-  constructor() { }
+  isLogged$: Observable<boolean> | null = null;
+  constructor(
+    private store: Store<ConnecticationStore>
+  ) { }
 
   ngOnInit(): void {
+    this.isLogged$ = this.store.select('auth')
+      .pipe(map(x => x.isLogged));
   }
-
 }
