bd819d52e5a755f3fbab649a87b1c67ed9ef44cb
Mikhail Istomin <mikhail_istomin@epam.com>
2022-11-17 00:32:42 +0400

    complete sign out

--- src/app/store/auth.effects.ts
+++ src/app/store/auth.effects.ts
@@ -1,17 +1,19 @@
 
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import {map, catchError, switchMap} from 'rxjs/operators';
+import {EMPTY, of} from 'rxjs';
+import {map, catchError, switchMap, tap} from 'rxjs/operators';
 import * as AuthActions from './auth.actions';
 import {UserService} from '../shared/user.service';
+import {AuthService} from '../shared/auth.service';
 
 @Injectable()
 export class AuthEffects {
 
   constructor(
     private actions$: Actions,
-    private userService: UserService
+    private userService: UserService,
+    private authService: AuthService,
   ) {}
 
   getCurrentUserProfile = createEffect(() =>
@@ -24,6 +26,15 @@ export class AuthEffects {
         );
       })
     )
+  );
+
+  signOut = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signOut),
+      tap(action => {
+        this.authService.signOut();
+      })
+    ),  {dispatch: false}
   );
 
 }
