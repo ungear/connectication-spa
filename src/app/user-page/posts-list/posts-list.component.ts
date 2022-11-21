7afdbee2dfca7ffda64d932fa2500109b28efc55
Mikhail Istomin <mikhail_istomin@epam.com>
2022-11-21 16:29:46 +0400

    Posts: update post list when post removed

--- src/app/user-page/posts-list/posts-list.component.ts
+++ src/app/user-page/posts-list/posts-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Post} from '../types/post.interface';
-import {merge, Observable, of} from 'rxjs';
+import {merge, Observable, of, Subject} from 'rxjs';
 import {PostService} from '../post.service';
 import {UserPageService} from '../user-page.service';
 import {switchMap} from 'rxjs/operators';
@@ -14,23 +14,27 @@ export class PostsListComponent implements OnInit {
   @Input() userId!: string;
   @Input() isOwner!: boolean;
   posts$: Observable<Post[]> | null = null;
+  private postDeletion$: Subject<any> = new Subject();
   constructor(
     private postService: PostService,
     private userPageService: UserPageService,
   ) {}
 
   ngOnInit(): void {
-    // fetch all posts right away AND when user creates a new post
+    // fetch all posts right away AND when user creates a new post AND when post deleted
     this.posts$ = merge(
       of(1),
-      this.userPageService.postCreated$
+      this.userPageService.postCreated$,
+      this.postDeletion$
     ).pipe(
       switchMap(() => this.postService.getUserPosts(this.userId))
     );
   }
 
   onDeleteClick(postId: number): void{
-    this.postService.deletePost(postId).subscribe();
+    this.postService.deletePost(postId).subscribe(() => {
+      this.postDeletion$.next(1);
+    });
   }
 
 }
